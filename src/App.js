8bb96b3c87d97a96930e0295253462cc3b08d547
2002Hung <hungchu101@gmail.com>
2023-02-08 09:15:23 +0700

    Make Modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.1.4/css/boxicons.min.css';
+import './App.scss';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import Catalog from './pages/Catalog';
+import Detail from './pages/Detail';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index path="/" element={<Home/>} />
+        <Route path="/:category" element={<Catalog />} />
+        <Route path="/:category/search/:keyword" element={<Catalog />} />
+        <Route path="/:category:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
